f3aa18d88bcac1e13f61948af82f861e5afe77ee
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2020-12-21 12:23:34 -0500

    linked javascript

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//javascript.js
+//central javascript file that is going to be the Hub of all the other files
+//This basically means that this will controll what all the other files do and make
+//
+//The reason for this is so that we can have multiple files that have their own purpose
+//but keep the simplicity of the uses in here
+//
+//Say we have a file specifically for the character and it contains a class to form it
+//we can use this to A.) create and B.) change what the character does
+//
+//I don't know and can't really tell if we are going to be doing it in all one file
+//or if Lamorrie is gunna have us do it spread out but either way its just a file
